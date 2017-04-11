7204afdbc368a98740382500f7817578df4760f2
Alvin Lu <alvinspeakstoo@gmail.com>
2017-04-10 23:48:53 -0700

    testing if postgres can be readd by server

--- index.js
+++ index.js
@@ -2,17 +2,14 @@ var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
 
+// Setting up module for PostgreSQL to be utilized in NodeJS
 var pg = require('pg');
-pg.defaults.ssl = true;
-var conString = (process.env.DATABASE_URL);
-var sess;
+// var connect = "postgres://alvinlu:Typical8!@localhost/temp_connect";
 
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
-
-// app.use(morgan('dev'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -26,27 +23,9 @@ app.get('/land', function(request, response) {
   response.render('pages/land');
 });
 
-app.post('/land', function(request, response){
-  pg.connect(conString, function(err, client, done) {
-	    if (err) {
-	      return console.error('error fetching client from pool', err);
-	    }
-	    console.log("connected to database");
-
-	    client.query('INSERT INTO temp_user(fname, lname) VALUES($1, $2)', [req.body.fname, req.body.lname], function(err, result) {
-	      done();
-	      if (err) {
-	        return console.error('error running query', err);
-	      }
-
-	      res.redirect('/db');
-	    });
-	});
-});
-
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
+    client.query('SELECT * FROM temp_user', function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
