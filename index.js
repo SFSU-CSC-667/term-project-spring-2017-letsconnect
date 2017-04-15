6b3bfba1d7bde59ba8e5f07fd1443e5daba809e2
Alvin Lu <alvinspeakstoo@gmail.com>
2017-04-15 14:13:19 -0700

    shows fname and lname

--- index.js
+++ index.js
@@ -5,16 +5,6 @@ var app = express();
 // Setting up module for PostgreSQL to be utilized in NodeJS
 var pg = require('pg');
 pg.defaults.ssl = true;
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  if (err) throw err;
-  console.log('Connected to postgres! Getting schemas...');
-
-  client
-    .query('SELECT table_schema,table_name FROM information_schema.tables;')
-    .on('row', function(row) {
-      console.log(JSON.stringify(row));
-    });
-});
 
 var conString = (process.env.DATABASE_URL);
 var sess;
