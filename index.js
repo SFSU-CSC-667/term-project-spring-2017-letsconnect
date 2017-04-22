f5119dc108e032e5c7f2566236628eb7ce156c51
mbain <mbain@mail.sfsu.edu>
2017-04-22 12:13:18 -0700

    Removed extra closing braces.

--- index.js
+++ index.js
@@ -35,32 +35,31 @@ app.post('/land', function(req, res){
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
 
     if(client)
-      console.log(client);
+    console.log(client);
     else
-      console.log("no client.")
-	    if (err) {
-	      return console.error('error fetching client from pool', err);
-	    }
-	    console.log("connected to database");
+    console.log("no client.")
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
+    console.log("connected to database");
 
-	    //client.query('INSERT INTO temp_user VALUES(, $1, $2)', [reqbody.fname, reqbody.lname], function(err, result) {
+    //client.query('INSERT INTO temp_user VALUES(, $1, $2)', [reqbody.fname, reqbody.lname], function(err, result) {
 
-	      if (err) {
-	        return console.error('error running query', err);
-	      }
-        done();
-	      res.redirect('/db');
-	    });
-	});
+    if (err) {
+      return console.error('error running query', err);
+    }
+    done();
+    res.redirect('/db');
+  });
 });
 
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM temp_user', function(err, result) {
       if (err)
-       { console.error(err); response.send("Error " + err); }
+      { console.error(err); response.send("Error " + err); }
       else
-       { response.render('pages/db', {results: result.rows} ); }
+      { response.render('pages/db', {results: result.rows} ); }
       done();
     });
   });
@@ -71,10 +70,10 @@ app.get('/cool', function(request, response) {
 });
 
 app.get('/times', function(request, response) {
-    var result = ''
-    var times = process.env.TIMES || 5
-    for (i=0; i < times; i++)
-      result += i + ' ';
+  var result = ''
+  var times = process.env.TIMES || 5
+  for (i=0; i < times; i++)
+  result += i + ' ';
   response.send(result);
 });
 
