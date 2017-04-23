c159de31f5b94ad083e18cb45945557deac9717f
Alvin Lu <alvinspeakstoo@gmail.com>
2017-04-23 15:30:29 -0700

    trying to make land the default landing page

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/land');
+  response.render('pages/index');
 });
 
 app.post('/', function(req, res){
