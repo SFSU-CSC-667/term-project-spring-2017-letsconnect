ea315a5dffeeff61473975f020a5210fa3765a6d
Alvin Lu <alvinspeakstoo@gmail.com>
2017-04-23 15:24:13 -0700

    uh

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-var cool = require('cool-ascii-faces');
+// var cool = require('cool-ascii-faces');
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-
 
 // Setting up module for PostgreSQL to be utilized in NodeJS
 var pg = require('pg');
@@ -69,17 +68,17 @@ app.get('/db', function (request, response) {
   });
 });
 
-app.get('/cool', function(request, response) {
-  response.send(cool());
-});
-
-app.get('/times', function(request, response) {
-  var result = ''
-  var times = process.env.TIMES || 5
-  for (i=0; i < times; i++)
-  result += i + ' ';
-  response.send(result);
-});
+// app.get('/cool', function(request, response) {
+//   response.send(cool());
+// });
+//
+// app.get('/times', function(request, response) {
+//   var result = ''
+//   var times = process.env.TIMES || 5
+//   for (i=0; i < times; i++)
+//   result += i + ' ';
+//   response.send(result);
+// });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
