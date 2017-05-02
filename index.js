ece2fa6a547c40d3268b3eb52c4a4b59cd4862d2
Alvin Lu <alvinspeakstoo@gmail.com>
2017-05-01 20:24:28 -0700

    landing page

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 // var cool = require('cool-ascii-faces');
 var express = require('express');
 var bodyParser = require('body-parser');
+var socketIO = require('socket.io');
+var path = require('path');
+var logger = require('morgan');
 var app = express();
-
 // Setting up module for PostgreSQL to be utilized in NodeJS
 var pg = require('pg');
 pg.defaults.ssl = true;
 
 var sess;
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 3000));
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -93,18 +94,6 @@ app.get('/db', function (request, response) {
     });
   });
 });
-
-// app.get('/cool', function(request, response) {
-//   response.send(cool());
-// });
-//
-// app.get('/times', function(request, response) {
-//   var result = ''
-//   var times = process.env.TIMES || 5
-//   for (i=0; i < times; i++)
-//   result += i + ' ';
-//   response.send(result);
-// });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
