92c34589aac690e51c1d27c58856b9e0df08fc33
Alvin Lu <alvinspeakstoo@gmail.com>
2017-05-14 15:01:28 -0700

    testing

--- index.js
+++ index.js
@@ -27,10 +27,10 @@ app.get('/', function(request, response) {
 
 app.post('/', function(req, res){
 
-  // console.log("Request body: " + req.body);
-  // console.log("First name: "+ req.body.fname);
-  // console.log("Last name:" + req.body.lname);
-  // console.log("Database URL: " + process.env.DATABASE_URL);
+  console.log("Request body: " + req.body);
+  console.log("First name: "+ req.body.fname);
+  console.log("Last name:" + req.body.lname);
+  console.log("Database URL: " + process.env.DATABASE_URL);
 
   var fname = req.body.fname;
   var lname = req.body.lname;
